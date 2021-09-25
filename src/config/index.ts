344cdc1787264b879dbf93b4364445bcc1751570
OlegLep <oleh.lepetchenko@gmail.com>
2021-09-25 21:31:08 +0300

    feat: add ability to use .env variables
    also update .gitignore (add there .env) and .eslintrc; also add .env.example with examples for
    application environment

---
+++ src/config/index.ts
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv';
+
+declare const process : {
+  env: {
+    [key: string]: string,
+  }
+};
+
+// Set the NODE_ENV to 'development' by default
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+const envFound = dotenv.config();
+if (envFound.error) {
+  // This error should crash whole process
+
+  throw new Error("⚠️  Couldn't find .env file  ⚠️");
+}
+
+export default {
+  port: parseInt(process.env.PORT, 10),
+};
