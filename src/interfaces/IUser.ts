f0f3d3cc6d46f0f66416b706f8f5f8acf73bb828
OlegLep <oleh.lepetchenko@gmail.com>
2021-09-29 18:09:09 +0300

    refactor(IUser interface): update fields in interface in accordance to returnable user

--- src/interfaces/IUser.ts
+++ src/interfaces/IUser.ts
@@ -1,8 +1,9 @@
 export interface IUser {
   _id: string;
-  name: string;
+  userName: string;
   email: string;
-  password: string;
+  createdAt: string;
+  updatedAt: string;
 }
 
 export interface IUserInputDTO {
