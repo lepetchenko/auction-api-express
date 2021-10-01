b2f673d677f445f5562887699fda66f84bfdc7b3
OlegLep <oleh.lepetchenko@gmail.com>
2021-10-01 18:34:29 +0300

    refactor(app bootsrtap): change way to bootstrap app
    use class syntax instead of functions. made changes for nodemon config

---
+++ src/App.ts
@@ -0,0 +1,43 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+import routes from '@/api/routes';
+import mongooseLoader from '@/loaders/mongoose';
+import config from '@/config';
+
+class App {
+  public app: express.Application;
+
+  public port: number;
+
+  constructor() {
+    this.app = express();
+    this.port = config.port;
+
+    this.initializeMiddlewares();
+    this.initializeRoutes();
+    this.initializeMongoose();
+  }
+
+  private initializeMiddlewares = () => {
+    this.app.use(bodyParser.urlencoded({ extended: true }));
+    this.app.use(bodyParser.json());
+  };
+
+  private initializeRoutes = () => {
+    this.app.use(routes());
+  };
+
+  private initializeMongoose = () => {
+    mongooseLoader();
+  };
+
+  public listen = () => {
+    this.app.listen(this.port, () => {
+      // eslint-disable-next-line no-console
+      console.log(`The application is listening on port ${this.port}!`);
+    });
+  };
+}
+
+export default App;
