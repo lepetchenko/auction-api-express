be4e8517ac3131b1aa41afb91164f20aad9f1142
OlegLep <oleh.lepetchenko@gmail.com>
2021-10-13 19:19:12 +0300

    feat(user model): add validations for values in user model fields

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,4 +1,5 @@
 import { Schema, Document, model } from 'mongoose';
+import Joi from 'joi';
 
 import { IUser } from '@/interfaces/IUser';
 
@@ -6,17 +7,31 @@ const userSchema = new Schema(
   {
     userName: {
       type: String,
-      required: [true, 'Please enter a name'],
+      required: [true, 'Please enter a user name'],
       index: true,
+      unique: true,
+      validate: [
+        (userName: string) => !Joi.string().min(3).max(30).required()
+          .validate(userName).error,
+        'Please fill a valid user name',
+      ],
     },
-    // TODO add validation
     email: {
       type: String,
+      required: [true, 'Please enter an email name'],
       lowercase: true,
       unique: true,
       index: true,
+      validate: [
+        (email: string) => !Joi.string().email().required()
+          .validate(email).error,
+        'Please fill a valid email address',
+      ],
     },
-    password: String,
+    password: {
+      type: String,
+      required: [true, 'Please enter a password'],
+    },
   },
   { timestamps: true },
 );
