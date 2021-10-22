e75c92077139645c9a3785e370f751a71062b852
OlegLep <oleh.lepetchenko@gmail.com>
2021-10-22 12:47:31 +0300

    refactor(handleerror): move 'handleError' from utils to meddlewares

---
+++ src/middlewares/handleError.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from 'express';
+
+/** Last 'next' param is needed to identify this function as error handler */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export default (error: any, req: Request, res: Response, next: NextFunction) => {
+  /** Process error as Boom instance error (handled and thrown manually from code) */
+  if (error.isBoom) {
+    const { output: { statusCode }, message } = error;
+    return res.status(statusCode).json({ message });
+  }
+
+  // TODO log(req.path, error.message)
+  console.error(req.path, error.message);
+  /**
+   * Process error as unknown error (for example, duplicate key im mongo). It means that we
+   * do not know what code thrown this error. In this case it's better to log what happened
+   * and hide error message from user (just use some abstract message, like 'Internal sever error'
+   * and some abstract error code, like 500)
+   */
+  return res.status(500).json({ message: 'Internal sever error' });
+};
