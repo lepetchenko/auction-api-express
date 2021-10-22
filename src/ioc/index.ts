a3a9d86cae6628df5d42595ce89c8d4392e84d8b
OlegLep <oleh.lepetchenko@gmail.com>
2021-10-22 12:38:03 +0300

    refactor(ioc, auth service): remove useless code

--- src/ioc/index.ts
+++ src/ioc/index.ts
@@ -3,17 +3,14 @@ import { Container } from 'inversify';
 import { Model } from 'mongoose';
 
 import AuthService from '@/services/AuthService';
-import MailService from '@/services/MailService';
 import UserModel from '@/models/user';
 import { eventBusInstance } from '@/decorators/EventBus';
 import { IAuthService } from '@/interfaces/IAuthService';
-import { IMailService } from '@/interfaces/IMailService';
 import { IEventBus } from '@/interfaces/IEventBus';
 import TYPES from '@/constants/types';
 
 const container = new Container();
 container.bind<IAuthService>(TYPES.services.AuthService).to(AuthService);
-container.bind<IMailService>(TYPES.services.MailService).to(MailService);
 container.bind<Model<any>>(TYPES.models.UserModel).toFunction(UserModel);
 container.bind<IEventBus>(TYPES.decorators.EventBus).toConstantValue(eventBusInstance);
 
--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -2,7 +2,6 @@ import { injectable, inject } from 'inversify';
 
 import { IUser, IUserInputDTO, IUserModel } from '@/interfaces/IUser';
 import { IAuthService } from '@/interfaces/IAuthService';
-import { IMailService } from '@/interfaces/IMailService';
 import { IEventBus } from '@/interfaces/IEventBus';
 import TYPES from '@/constants/types';
 import EVENTS from '@/constants/events';
@@ -11,18 +10,14 @@ import EVENTS from '@/constants/events';
 export default class AuthService implements IAuthService {
   public constructor(
   // eslint-disable-next-line @typescript-eslint/indent
-    @inject(TYPES.services.MailService) private mailService: IMailService,
     @inject(TYPES.models.UserModel) private userModel: IUserModel,
     @inject(TYPES.decorators.EventBus) private eventBus: IEventBus,
   ) {
-    this.mailService = mailService;
     this.userModel = userModel;
     this.eventBus = eventBus;
   }
 
-  public signUp = async (
-    userInputDTO: IUserInputDTO,
-  ): Promise<{ user: IUser }> => {
+  public signUp = async (userInputDTO: IUserInputDTO): Promise<{ user: IUser }> => {
     const { userName, password, email } = userInputDTO;
 
     const user = await this.userModel.checkAndCreate({
