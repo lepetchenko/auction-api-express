2224fe3d26cde11c3874c820311d8458a1957de5
OlegLep <oleh.lepetchenko@gmail.com>
2021-10-27 20:11:30 +0300

    refactor(tests/mocks/express): make code more readable

--- src/tests/mocks/express.ts
+++ src/tests/mocks/express.ts
@@ -1,11 +1,9 @@
 import { Response, Request } from 'express';
 
-export const mockResponse = () => {
-  const res: Partial<Response> = {};
-  res.status = jest.fn().mockReturnValue(res);
-  res.json = jest.fn().mockReturnValue(res);
-  return res;
-};
+export const mockResponse = () => ({
+  status: jest.fn().mockReturnThis(),
+  json: jest.fn().mockReturnThis(),
+} as Partial<Response>);
 
 export const mockRequest = (requestContent?: any) => ({
   ...requestContent,
