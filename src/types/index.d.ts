2c647bf552271296350dfd57d4d9e318d51d1f43
OlegLep <oleh.lepetchenko@gmail.com>
2022-06-18 15:51:44 +0300

    chore(types): upd types for req.user

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,9 +1,11 @@
+import { IUserDocument } from '@/interfaces/IUser';
+
 export {};
 
 declare global {
   namespace Express {
     interface Request {
-      user: any
+      user: IUserDocument
     }
   }
 }
